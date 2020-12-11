031cba7ebf1bf0a3df937532650f862d0fe2399d
万松涛 <2294215581@qq.com>
2020-12-11 12:25:05 +0800

    修改获取用户信息的接口

--- src/api/user.js
+++ src/api/user.js
@@ -8,9 +8,9 @@ export function login(data) {
   })
 }
 
-export function getInfo(token) {
+export function getUserInfo(token) {
   return request({
-    url: '/vue-admin-template/user/info',
+    url: '/admin/getuserinfo',
     method: 'get',
     params: { token }
   })
