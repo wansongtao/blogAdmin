e76e4024e70b3f56fbfda4ec421e578f3e55e718
万松涛 <2294215581@qq.com>
2020-12-11 12:07:10 +0800

    修改拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
+import { Message } from 'element-ui'
 import store from '@/store'
-import { getToken } from '@/utils/auth'
 
 // 创建axios实例
 const service = axios.create({
@@ -13,13 +12,13 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     if (store.getters.token) {
-      // 让每个请求携带请求头
-      config.headers['Authorization'] = getToken()
+      // 让每个请求携带请求头, 加上Bearer
+      config.headers['Authorization'] = `Bearer ${store.getters.token}`
     }
     return config
   },
   error => {
-    console.log(error)
+    console.error(error)
     return Promise.reject(error)
   }
 )
@@ -28,40 +27,51 @@ service.interceptors.request.use(
 service.interceptors.response.use(
 
   response => {
-    const res = response.data
+    const { success, code, data, message } = response.data
 
-    // 拦截一些特殊的错误状态码，统一的异常处理
-    if (res.code !== 20000) {
-      Message({
-        message: res.message || 'Error',
-        type: 'error',
-        duration: 5 * 1000 // 持续时间
-      })
-
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // to re-login
-        MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
-          confirmButtonText: '重新登录',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload() // 为了重新实例化vue-router对象，防止bug
-          })
-        })
-      }
-      return Promise.reject(new Error(res.message || 'Error'))
-    } else {
-      return res
+    if (success) {
+      return data
     }
+
+    if (code === 500) {
+      Message.error(message || '用户身份过期，请重新登录')
+      return Promise.reject(new Error(message || '服务器错误'))
+    }
+
+    Message.error(message || '操作失败')
+    return Promise.reject(new Error(message || '服务器错误'))
+    // 拦截一些特殊的错误状态码，统一的异常处理
+    // if (res.code !== 20000) {
+    //   Message({
+    //     message: res.message || 'Error',
+    //     type: 'error',
+    //     duration: 5 * 1000 // 持续时间
+    //   })
+
+    //   // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
+    //   if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+    //     // to re-login
+    //     MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
+    //       confirmButtonText: '重新登录',
+    //       cancelButtonText: '取消',
+    //       type: 'warning'
+    //     }).then(() => {
+    //       store.dispatch('user/resetToken').then(() => {
+    //         location.reload() // 为了重新实例化vue-router对象，防止bug
+    //       })
+    //     })
+    //   }
+    //   return Promise.reject(new Error(res.message || 'Error'))
+    // } else {
+    //   return res
+    // }
   },
   error => {
-    console.log('err' + error)
+    console.error('err' + error)
     Message({
       message: error.message,
       type: 'error',
-      duration: 5 * 1000
+      duration: 3 * 1000
     })
     return Promise.reject(error)
   }
