1c2d6433ad99d41515f2731e7be213d078e8d674
万松涛 <2294215581@qq.com>
2020-12-11 21:03:08 +0800

    修改头部导航栏和侧边栏样式

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -69,7 +69,7 @@ export default {
   height: 50px;
   overflow: hidden;
   position: relative;
-  background: #fff;
+  background-image: linear-gradient(to right, #eae5c9, #6cc6cb);
   box-shadow: 0 1px 4px rgba(0,21,41,.08);
 
   .hamburger-container {
--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -5,7 +5,6 @@
       <el-menu
         :default-active="activeMenu"
         :collapse="isCollapse"
-        :background-color="variables.menuBg"
         :text-color="variables.menuText"
         :unique-opened="false"
         :active-text-color="variables.menuActiveText"
