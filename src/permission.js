183a21ba7b424a91a3cd724116a84d958549212f
万松涛 <2294215581@qq.com>
2020-12-11 12:02:05 +0800

    修改路由守卫

--- src/permission.js
+++ src/permission.js
@@ -1,30 +1,30 @@
 import router from './router'
 import store from './store'
 import { Message } from 'element-ui'
-import NProgress from 'nprogress' // progress bar
+import NProgress from 'nprogress' // 进度条
 import 'nprogress/nprogress.css' // progress bar style
-import { getToken } from '@/utils/auth' // get token from cookie
+// import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
-const whiteList = ['/login'] // no redirect whitelist
+const whiteList = ['/login', '/404'] // 白名单
 
+// 路由守卫
 router.beforeEach(async(to, from, next) => {
-  // start progress bar
+  // 开始进度条
   NProgress.start()
 
   // set page title
   document.title = getPageTitle(to.meta.title)
 
-  // determine whether the user has logged in
-  const hasToken = getToken()
+  // 获取token
+  const hasToken = store.getters.token
 
   if (hasToken) {
     if (to.path === '/login') {
-      // if is logged in, redirect to the home page
+      // 有token代表用户已经登录了，直接跳到首页
       next({ path: '/' })
-      NProgress.done()
     } else {
       const hasGetUserInfo = store.getters.name
       if (hasGetUserInfo) {
@@ -40,22 +40,23 @@ router.beforeEach(async(to, from, next) => {
           await store.dispatch('user/resetToken')
           Message.error(error || 'Has Error')
           next(`/login?redirect=${to.path}`)
-          NProgress.done()
         }
       }
     }
   } else {
-    /* has no token*/
+    /* 没有token*/
 
     if (whiteList.indexOf(to.path) !== -1) {
-      // in the free login whitelist, go directly
+      // 在白名单里面，放行
       next()
     } else {
-      // other pages that do not have permission to access are redirected to the login page.
+      // 无权限访问，重定向到登录页面，并加上请求字符串记录用户是从哪个页面跳转到登录页的方便登录后跳转到对应的页面
       next(`/login?redirect=${to.path}`)
-      NProgress.done()
     }
   }
+
+  // 手动关闭一次，解决手动切换地址时，进度条不关闭的问题
+  NProgress.done()
 })
 
 router.afterEach(() => {
